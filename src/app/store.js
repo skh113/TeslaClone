ade6464560c1665ef2b5a8f932826bc10415a3f5
SKH <skhprogramming@gmail.com>
2022-01-18 23:18:25 +0330

    ADD: animations and menu.

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
